412a17902a6c262a0333328040d4e1d3fd3dbcf5
siddhantkumar024 <76165102+siddhantkumar024@users.noreply.github.com>
2020-12-17 18:41:51 +0530

    with sample output

--- robins01.py
+++ robins01.py
@@ -31,3 +31,38 @@ while (userguess != randomnumber):
             print('New range-:',l,'-',s)
             print('\n')
 print(f"You Guessed the number in {guesses} guesses")
+
+
+'''
+output be like this
+
+
+(compiler- 97
+Enter the guess-100
+Guess 1: 100
+Try Again! You guessed too high
+New range-: 0 - 100
+
+
+Enter the guess-80
+Guess 2: 80
+Try Again! You guessed too low
+New range-: 80 - 100
+
+
+Enter the guess-85
+Guess 3: 85
+Try Again! You guessed too low
+New range-: 85 - 100
+
+
+Enter the guess-95
+Guess 4: 95
+Try Again! You guessed too low
+New range-: 95 - 100
+
+
+Enter the guess-97
+Guess 5: 97
+Right number)
+'''
